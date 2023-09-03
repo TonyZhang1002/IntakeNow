c7b94aea1b5dba6a5b8d9071e967d3c167c98d18
TonyZhang1002 <z496722204a@hotmail.com>
2023-09-03 11:52:18 +0100

    Initial commit

---
+++ src/main/java/com/example/intakenow/IntakeNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.intakenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntakeNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntakeNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intakenow/IntakeNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intakenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntakeNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
