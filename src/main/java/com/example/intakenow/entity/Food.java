605aa677d2c0edf79ca4241d04a1f66599eb7ce5
TonyZhang1002 <z496722204a@hotmail.com>
2023-09-03 13:17:26 +0100

    cleanup

---
+++ src/main/java/com/example/intakenow/entity/Food.java
@@ -0,0 +1,31 @@
+package com.example.intakenow.entity;
+
+public class Food {
+    private String name;
+    private int price;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
